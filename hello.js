72c6d3879b33c3c3624a678614c0df4171f2328c
loganrains <logankrains@gmail.com>
2024-03-16 19:28:48 -0500

    now says Hello World

--- hello.js
+++ hello.js
@@ -3,20 +3,29 @@ Welcome, new coder.
 Enter your code on line 7 to print 'Hello, World!' 
 to the console (the panel on the right)
 */
+console.log("Hello, World!");
 
-console.log("Hello");
+console.log(14 + 7);
+
+console.log("^ is my favorite number"); console.log("^ is correct");
+
+console.log("^ is correct");
+
+console.log('Quoth the Raven "Nevermore".');
+
+console.log("Assignment" + " " + "Complete");
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
-   9. Other. You choose!
+   1. Change the message that is printed. X
+   2. Figure out what the parentheses do. Will the code work without them? X - it will not run without
+   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")? X - They appear interchangeable
+   4. Remove the semi-colon, ;. X - Nice, I tend to forget these
+   5. Print a number. (Bonus: Print two numbers added together). X 
+   6. Print multiple messages one after the other. X
+   7. Print two messages on the same line. X
+   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore". X
+   9. Other. You choose! X
 */
